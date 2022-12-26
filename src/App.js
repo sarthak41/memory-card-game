217917bdb5fa58d95219b5a628648352f5a605db
Sarthak Gharat <sgharat0903@gmail.com>
2022-12-26 21:44:20 +0530

    Add cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Card from "./components/Card";
+import geraltImg from "./images/geralt-of-rivia.jpg";
+import yenImg from "./images/yennefer-of-vengerberg.jpg";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Card image={geraltImg} title="Geralt of Rivia" />
+      <Card image={yenImg} title="Yennefer" />
+    </div>
+  );
 }
 
 export default App;
